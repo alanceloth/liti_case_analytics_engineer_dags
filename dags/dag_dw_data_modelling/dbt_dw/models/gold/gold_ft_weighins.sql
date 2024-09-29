fcbe3784be1242456265f64dbc4295f4792c571d
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-09-29 19:57:23 -0300

    add(dbt): adicionado novas colunas na gold_ft_weighins

--- dags/dag_dw_data_modelling/dbt_dw/models/gold/gold_ft_weighins.sql
+++ dags/dag_dw_data_modelling/dbt_dw/models/gold/gold_ft_weighins.sql
@@ -9,10 +9,18 @@ WITH customer_dim AS (
 ),
 weighins AS (
   SELECT
-    wheiginId AS weighin_id,
+    weighinId AS weighin_id,
     owner_id AS customer_id,
     CAST(createdAt AS DATE) AS weighin_date,
-    weightkg AS weight
+    weightkg AS weight,
+    bodyFatPercent AS body_fat_percent,
+    musclePercent AS muscle_percent,
+    physicalAge AS physical_age,
+    bmiStandard AS bmi,
+    
+    -- Calculando a variação de peso (em relação à pesagem anterior)
+    LAG(weightkg, 1) OVER (PARTITION BY owner_id ORDER BY CAST(createdAt AS DATE)) AS previous_weight,
+    weightkg - LAG(weightkg, 1) OVER (PARTITION BY owner_id ORDER BY CAST(createdAt AS DATE)) AS weight_variation
   FROM {{ ref('silver_weighins') }}
 )
 SELECT
@@ -23,6 +31,11 @@ SELECT
   c.customer_birth_date,
   c.origin_channel_group,
   w.weighin_date,
-  w.weight
+  w.weight,
+  w.body_fat_percent,
+  w.muscle_percent,
+  w.physical_age,
+  w.bmi,
+  w.weight_variation
 FROM weighins w
-JOIN customer_dim c ON w.customer_id = c.customer_id
+JOIN customer_dim c ON w.customer_id = c.customer_id;
